1d6fe3a8ea0af1ce25e4fdbc84016250a102ac57
Alexa VanHooser <alexavanhooser@gmail.com>
2017-10-19 15:48:10 -0700

    added pipe folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
+import { SearchByPipe } from './pipe/search-by-pipe';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchByPipe
   ],
   imports: [
     BrowserModule,
